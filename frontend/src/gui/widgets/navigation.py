bf9f31f5c5800dfc2652c9ec7bcc8234e9512a69
Cường Dev <154894942+cuongnadev@users.noreply.github.com>
2025-09-19 19:45:57 +0700

    refactor: navigation

--- frontend/src/gui/widgets/navigation.py
+++ frontend/src/gui/widgets/navigation.py
@@ -1,4 +1,3 @@
-import os
 from PySide6.QtWidgets import (
     QVBoxLayout, QWidget, QLabel, QPushButton, QSizePolicy
 )
@@ -8,6 +7,8 @@ from pathlib import Path
 
 from utils.happers import RoundedPixmap
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ASSETS_DIR = BASE_DIR / "assets"
 class Navigation(QWidget):
     def __init__(self):
         super().__init__()
@@ -24,30 +25,68 @@ class Navigation(QWidget):
 
         # avatar
         self.avatar = QLabel()
-        BASE_DIR = Path(__file__).resolve().parent.parent.parent
-        ASSETS_DIR = BASE_DIR / "assets"
         avatar_path = str(ASSETS_DIR / "avatarC.png")
         self.avatar.setPixmap(RoundedPixmap(avatar_path, 42, 0.2))
-        self.avatar.setPixmap(RoundedPixmap(avatar_path, 42))
         self.avatar.setAlignment(Qt.AlignCenter)
         main_layout.addWidget(self.avatar)
+
+        # home
+        self.home = QPushButton()
+        self.home.setFixedSize(40, 40)
+        home_path = str(ASSETS_DIR / "home.svg")
+        self.home.setIcon(QIcon(home_path))
+        self.home.setIconSize(QSize(24, 24))
+        self.home.setStyleSheet("""
+            QPushButton {
+                border-radius: 20px;
+                border: none;
+            }
+            QPushButton:hover {
+                background-color: rgba(97, 94, 240, 0.10);
+            }
+        """)
+
+        # messages
+        self.messages = QPushButton()
+        self.messages.setFixedSize(40, 40)
+        messages_path = str(ASSETS_DIR / "messages.svg")
+        self.messages.setIcon(QIcon(messages_path))
+        self.messages.setIconSize(QSize(24, 24))
+        self.messages.setStyleSheet("""
+            QPushButton {
+                border-radius: 20px;
+                border: none;
+            }
+            QPushButton:hover {
+                background-color: rgba(97, 94, 240, 0.10);
+            }
+        """)
+
+        nav_layout = QVBoxLayout()
+        nav_layout.setContentsMargins(0, 10, 0, 10)
+        nav_layout.setSpacing(10)
+        nav_layout.setAlignment(Qt.AlignCenter)
+
+        nav_layout.addWidget(self.home)
+        nav_layout.addWidget(self.messages)
+
+        main_layout.addLayout(nav_layout)
 
         main_layout.addStretch()
 
         # setting
         self.settings = QPushButton()
-        self.settings.setFixedSize(48, 48)
-        setting_path = str(ASSETS_DIR / "settings.svg")
+        self.settings.setFixedSize(40, 40)
+        setting_path = str(ASSETS_DIR / "gear.svg")
         self.settings.setIcon(QIcon(setting_path))
-        self.settings.setIconSize(QSize(32, 32))
+        self.settings.setIconSize(QSize(24, 24))
         self.settings.setStyleSheet("""
             QPushButton {
-                border-radius: 24px;
+                border-radius: 20px;
                 border: none;
-                color: white;
             }
             QPushButton:hover {
-                background-color: #1b6ae0;
+                background-color: rgba(97, 94, 240, 0.10);
             }
         """)
         main_layout.addWidget(self.settings, alignment=Qt.AlignCenter)
